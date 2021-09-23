1febb66e24669487f1423e0b8ee8b0d922f5d1d2
fernando <ferrafterminador@gmail.com>
2021-09-22 23:09:56 -0600

    Creando Gráficas con Python

---
+++ consultas.py
@@ -0,0 +1,35 @@
+import pymongo
+import regex as re
+import getpass
+import plotly.express as px 
+user=getpass.getuser() # obtener el usuario 
+url='C:\\Users\\'+user+'\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History' # url para obtener el historial
+client = pymongo.MongoClient("mongodb+srv://m001-student:1234@cluster0.lo2p9.mongodb.net/library?retryWrites=true&w=majority")
+collection = client.datos.historial
+datos = []
+for x in client.datos.historial.find(): 
+    datos.append(x)
+    print(x)
+print(len(datos))
+contadorFacebook = 0
+contadorLandivar = 0
+contadorYoutube = 0
+for x in datos:
+    if re.search('^https://www.facebook', x["url"]):
+        print(x)
+        contadorFacebook+=1
+    elif(re.search('^https://www.url', x["url"])):
+        print(x)
+        contadorLandivar+=1
+    elif(re.search('^https://www.youtube', x["url"])):
+        print(x)
+        contadorYoutube+=1
+print("La cantidad de veces visitado Facebook es ",contadorFacebook)
+print("La cantidad de veces visitado la página de la universidad es ",contadorLandivar)
+print("La cantidad de veces visitado la página de Youtube es ",contadorYoutube)
+random_x = [contadorFacebook, contadorLandivar, contadorYoutube]
+names = ['Facebook', 'Landivar', 'Youtube']
+ 
+fig = px.pie(values=random_x, names=names)
+fig.show()
+    
