b953d4defdb83f113f846c8d43324f6e14c0e84e
fernando <ferrafterminador@gmail.com>
2021-09-23 13:08:00 -0600

    terminando consultas

--- consultas.py
+++ consultas.py
@@ -1,7 +1,7 @@
 import pymongo
 import regex as re
 import getpass
-import plotly.express as px 
+import plotly.express as px
 user=getpass.getuser() # obtener el usuario 
 url='C:\\Users\\'+user+'\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History' # url para obtener el historial
 client = pymongo.MongoClient("mongodb+srv://m001-student:1234@cluster0.lo2p9.mongodb.net/library?retryWrites=true&w=majority")
@@ -14,7 +14,10 @@ print(len(datos))
 contadorFacebook = 0
 contadorLandivar = 0
 contadorYoutube = 0
+contadorGoogle = 0
+contador = 0
 for x in datos:
+    contador +=1
     if re.search('^https://www.facebook', x["url"]):
         print(x)
         contadorFacebook+=1
@@ -24,12 +27,14 @@ for x in datos:
     elif(re.search('^https://www.youtube', x["url"])):
         print(x)
         contadorYoutube+=1
+    elif(re.search('^https://www.google', x["url"])):
+        print(x)
+        contadorGoogle+=1
 print("La cantidad de veces visitado Facebook es ",contadorFacebook)
 print("La cantidad de veces visitado la página de la universidad es ",contadorLandivar)
 print("La cantidad de veces visitado la página de Youtube es ",contadorYoutube)
-random_x = [contadorFacebook, contadorLandivar, contadorYoutube]
-names = ['Facebook', 'Landivar', 'Youtube']
- 
+print("La cantidad de veces visitado la página de Youtube es ",contadorGoogle)
+random_x = [contadorFacebook, contadorLandivar, contadorYoutube, contadorGoogle]
+names = ['Facebook', 'Landivar', 'Youtube', 'Google'] 
 fig = px.pie(values=random_x, names=names)
 fig.show()
-    
